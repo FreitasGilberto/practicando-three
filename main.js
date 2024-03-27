e2653eaaf42693131b677454be353412717a3ed3
Gilberto Freitas <freitasbgilberto@gmail.com>
2024-03-26 23:55:38 -0300

    added orbit control

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 // The default rotation unit in Three.js is _____.
-//
-
 // You establish a parent-child relationship by adding an object to another object.
 
 function init() {
   var scene = new THREE.Scene();
 
-  scene.fog = new THREE.FogExp2(0xffffff, 0.2);
+  var gui = new dat.GUI();
+
+  var enableFog = false;
+
+  if (enableFog) {
+    scene.fog = new THREE.FogExp2(0xffffff, 0.2);
+  }
 
   var box = getBox(1, 1, 1);
   var plane = getPlane(20);
+  var pointLight = getPointLight(1);
+  var sphere = getSphere(0.05);
 
   plane.name = "plane-1";
 
@@ -22,9 +29,15 @@ function init() {
   // Radians is the default unit for rotation in Three.js.
 
   plane.rotation.x = Math.PI / 2;
+  pointLight.position.y = 2;
+  pointLight.intensity = 2;
+  gui.add(pointLight, "intensity", 0, 10);
+  gui.add(pointLight.position, "y", 0, 5);
 
   scene.add(box);
   scene.add(plane);
+  pointLight.add(sphere);
+  scene.add(pointLight);
 
   var camera = new THREE.PerspectiveCamera(
     45,
@@ -43,15 +56,18 @@ function init() {
 
   var renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setClearColor(0xffffff);
+  renderer.setClearColor("rgb(120, 120, 120)");
   document.getElementById("webgl").appendChild(renderer.domElement);
-  update(renderer, scene, camera);
+
+  var controls = new OrbitControls(camera, renderer.domElement);
+
+  update(renderer, scene, camera, controls);
 }
 
 function getBox(w, h, d) {
   var geometry = new THREE.BoxGeometry(w, h, d);
-  var material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
+  var material = new THREE.MeshPhongMaterial({
+    color: "rgb(120, 120, 120)",
   });
 
   // You need to provide a mesh with a geometry and a material to define it.
@@ -62,8 +78,8 @@ function getBox(w, h, d) {
 
 function getPlane(size) {
   var geometry = new THREE.PlaneGeometry(size, size);
-  var material = new THREE.MeshBasicMaterial({
-    color: 0xff0000,
+  var material = new THREE.MeshPhongMaterial({
+    color: "rgb(120, 120, 120)",
     side: THREE.DoubleSide,
   });
 
@@ -71,14 +87,32 @@ function getPlane(size) {
   return mesh;
 }
 
-function update(renderer, scene, camera) {
+function getSphere(size) {
+  var geometry = new THREE.SphereGeometry(size, 24, 24);
+  var material = new THREE.MeshBasicMaterial({
+    color: "rgb(250, 250, 250)",
+  });
+
+  // You need to provide a mesh with a geometry and a material to define it.
+
+  var mesh = new THREE.Mesh(geometry, material);
+  return mesh;
+}
+
+function getPointLight(intensity) {
+  var light = new THREE.PointLight(0xffffff, intensity);
+  return light;
+}
+
+function update(renderer, scene, camera, controls) {
   renderer.render(scene, camera);
+  controls.update();
 
   // Using the requestAnimationFrame function we are able to do continuous rendering in Three.js
 
   requestAnimationFrame(function () {
-    update(renderer, scene, camera);
+    update(renderer, scene, camera, controls);
   });
 }
 
-var scene = init();
+init();
